e0c9940a32170b5fabaa6107c0ad7f59260223c6
boodovskiy <dzak1987@mail.ru>
2021-07-12 11:42:22 +0300

    Add use cases for lesson one

---
+++ index_balances.php
@@ -0,0 +1,26 @@
+<?php
+	$apiKey = "YOUR_API_KEY";
+	$apiUrl = "https://api.covalenthq.com/";
+	$apiVersion = "v1";
+
+	$chainId = 1; //eth network
+	$ethAdress ="0xacd2222f84a713ddfadffccfa866a7eae3e25014";
+
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, $apiUrl . $apiVersion . '/' . $chainId . '/address/' . $ethAdress . '/transactions_v2/');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+	curl_setopt($ch, CURLOPT_USERPWD, $apiKey . ':' . '');
+
+	$result = curl_exec($ch);
+	if (curl_errno($ch)) {
+		echo 'Error:' . curl_error($ch);
+	}
+	
+	echo "<pre>";
+	var_dump(json_decode($result, true) );
+	
+	curl_close($ch);
+?>
